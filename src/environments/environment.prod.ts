86d9ce178fd070669dfffccda5f489a18d7f1323
Raj <rajju.14u@gmail.com>
2023-02-18 11:57:17 +0530

    initail files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  luv2shopApiUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51MS2TvSCcLSFN6vhS0eXD9p3RGB6KfWwwZc7Q2bq8uU70McFZQqgsOi0LrVdpd3eim58eWtWwcCYuwTWIg3rGX9W00kwkydVe0"
+};
