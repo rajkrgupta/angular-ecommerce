b11fdb9cf066adcc7fc3e1ea88bb0bf40ce2ba7f
rajkrgupta <rajju.14u@gmail.com>
2023-02-20 17:21:52 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  luv2shopApiUrl: "https://localhost:8443/api",
+  luv2shopApiUrl: "http://ecomserver-env.eba-irz2dcux.us-east-1.elasticbeanstalk.com/api",
   stripePublishableKey: "pk_test_51MS2TvSCcLSFN6vhS0eXD9p3RGB6KfWwwZc7Q2bq8uU70McFZQqgsOi0LrVdpd3eim58eWtWwcCYuwTWIg3rGX9W00kwkydVe0"
 };
 
